57cab93e5413a4ee5aa491f03a4581c8e899c343
Juniva616 <juniva616@gmail.com>
2022-10-04 15:57:59 +0000

    Make game_intro func setting the board size and ships number

--- run.py
+++ run.py
@@ -1,29 +1,43 @@
 import random
 
+def game_intro():
+    """
+    Receives name and area from user's input. Calculates size and ships_number.
+    """
+    name = input("Please, insert your name: \n")  
+    print()
+    print(f"Hello, {name}!")
 
-name = input("Please, insert your name: \n")  
+    while True:
+        print("There are 3 sizes of the game board: 4 x 4 = 16, 5 x 5 = 25 and 6 x 6 = 36")
+        area = int(input("To choose the board area, enter one of the numbers 16, 25 or 36: \n"))   
+        player = True
+        if area == 16:
+            size = 4
+            ships_num = 6
+            break
+        elif area == 25:
+            size = 5
+            ships_num = 10   
+            break
+        elif area == 36:
+            size = 6
+            ships_num = 13  
+            break   
+        else:
+            print("Wrong input! Please enter one of these numbers: 16, 25, 36")
+         
+        
+    print("!!!----> REMEMBER! If you want to stop the game, press CTRL+C <----!!!!!")
+        
+    return name, size, player, ships_num
+
+name, size, player, ships_num = game_intro()
 print()
-print(f"Hello, {name}!")
-print("There are 3 sizes of the game board: 4 x 4 = 16, 5 x 5 = 25 and 6 x 6 = 36")
-area = input("To choose the area, enter one of this numbers 16, 25 or 36: \n") 
-if area == 16:
-    size = 4
-    ships_num = 6   
-    Board(name, area, player, ships_num) 
-elif area == 25:
-    size = 5
-    ships_num = 10 
-    Board(name, area, player, ships_num)  
-elif area == 36:
-    size = 6
-    ships_num = 13   
-    Board(name, area, player, ships_num)    
-else:
-    print("Please enter one of these numbers: 16, 25, 36")
+print(f"Dear {name}, you start the game on the {size}x{size} board with {ships_num} ships!")
+print()
 
-print("REMEMBER! If you want to stop the game, press CTRL+C ")
 
-player = True
 
 
 
