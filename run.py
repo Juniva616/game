a026781049aed7e22b5d6ce7abe122b7ced5f560
Juniva616 <juniva616@gmail.com>
2022-10-04 21:34:23 +0000

    Wrap code into game_intro() function, receiving and validating user's input

--- run.py
+++ run.py
@@ -1,47 +1,38 @@
-import random
-
 def game_intro():
     """
     Receives name and area from user's input. Calculates size and ships_number.
     """
-    name = input("Please, insert your name: \n")  
+    name = input("Please, insert your name: \n")
     print()
-    print(f"Hello, {name}!")
+    print(f"Hello, {name}! To stop the game press CTRL+C, otherwise we continue.")  
+    print()
 
     while True:
-        print("There are 3 sizes of the game board: 4 x 4 = 16, 5 x 5 = 25 and 6 x 6 = 36")
-        area = int(input("To choose the board area, enter one of the numbers 16, 25 or 36: \n"))   
-        player = True
-        if area == 16:
-            size = 4
-            ships_num = 6
-            break
-        elif area == 25:
-            size = 5
-            ships_num = 10   
-            break
-        elif area == 36:
-            size = 6
-            ships_num = 13  
-            break   
-        else:
-            print("Wrong input! Please enter one of these numbers: 16, 25, 36")
-         
-        
-    print("!!!----> REMEMBER! If you want to stop the game, press CTRL+C <----!!!!!")
-        
+        try:
+            print("There are 3 sizes of the game board: 4 x 4 = 16, 5 x 5 = 25 and 6 x 6 = 36")
+            area = int(input("Please, choose one of the numbers 16, 25 or 36: \n"))
+            player = True
+            if area == 16:
+                size = 4
+                ships_num = 6
+                break
+            elif area == 25:
+                size = 5
+                ships_num = 10
+                break
+            elif area == 36:
+                size = 6
+                ships_num = 13
+                break
+            else:
+                raise ValueError()
+        except ValueError as e:
+            print("Invalid data! Please try again.")          
+
     return name, size, player, ships_num
+
 
 name, size, player, ships_num = game_intro()
 print()
 print(f"Dear {name}, you start the game on the {size}x{size} board with {ships_num} ships!")
 print()
-
-
-
-
-
-
-
-#print(random.randint(0, 3))
-
